0a0c2eccaf3391799d9d4482bf292858ab67f708
Roc Wang <roc@kiwiberry.nz>
2020-11-24 02:32:51 +1300

    feat: rerender homepage when viewport size changes

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,24 @@
   <button :class="[$style.container, $style.bgBanknote]" type="button">
     <CnyLandscape
       :class="$style.banknote"
-      v-for="n in numberOfItem"
-      :key="n"
-      :style="banknoteStyles[n]"
+      v-for="(style, i) in redEnvelopStyles"
+      :key="i"
+      :style="style"
     />
   </button>
 
   <button :class="[$style.container, $style.bgRedEnvelope]" type="button">
     <RedEnvelope
       :class="$style.redEnvelope"
-      v-for="n in numberOfItem"
-      :key="n"
-      :style="redEnvelopStyles[n]"
+      v-for="(style, i) in redEnvelopStyles"
+      :key="i"
+      :style="style"
     />
   </button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, onUnmounted } from "vue";
 import Head from "./components/Head.vue";
 import RedEnvelope from "./components/RedEnvelope.vue";
 import CnyLandscape from "./components/CnyLandscape.vue";
@@ -34,16 +34,26 @@ export default defineComponent({
     CnyLandscape,
   },
   setup() {
-    function getRandomStyle(n: number, minLength: number) {
+    interface Style {
+      transform: string;
+      zIndex: number;
+    }
+
+    function getRandomStyle(
+      n: number,
+      minLength: number,
+      viewportWidth: number,
+      viewportHeight: number
+    ): Style[] {
       return new Array(n).fill(null).map((item, index) => {
         // -minLength to 100vw + minLength
         const x = Math.floor(
-          (minLength + window.innerWidth) * Math.random() - minLength
+          (minLength + viewportWidth) * Math.random() - minLength
         );
 
         // -minLength to 50vh + minLength
         const y = Math.floor(
-          (minLength + window.innerHeight / 2) * Math.random() - minLength
+          (minLength + viewportHeight / 2) * Math.random() - minLength
         );
 
         // -90 to 90 deg
@@ -57,11 +67,36 @@ export default defineComponent({
     }
 
     const numberOfItem = 100;
+    const banknoteStyles = ref<Style[]>([]);
+    const redEnvelopStyles = ref<Style[]>([]);
+    const observer = new ResizeObserver((entries) => {
+      let width = window.innerWidth;
+      let height = window.innerHeight;
+
+      if (entries[0].borderBoxSize) {
+        const { inlineSize, blockSize } = entries[0].borderBoxSize[0]
+          ? entries[0].borderBoxSize[0]
+          : ((entries[0].borderBoxSize as unknown) as ResizeObserverSize);
+        width = inlineSize;
+        height = blockSize;
+      } else if (entries[0].contentRect) {
+        width = entries[0].contentRect.width;
+        height = entries[0].contentRect.height;
+      }
+
+      banknoteStyles.value = getRandomStyle(numberOfItem, 206, width, height);
+      redEnvelopStyles.value = getRandomStyle(numberOfItem, 188, width, height);
+    });
+
+    observer.observe(document.body, { box: "border-box" });
+    onUnmounted(() => {
+      observer.disconnect();
+    });
 
     return {
       numberOfItem,
-      banknoteStyles: getRandomStyle(numberOfItem, 206),
-      redEnvelopStyles: getRandomStyle(numberOfItem, 188),
+      banknoteStyles,
+      redEnvelopStyles,
     };
   },
 });
