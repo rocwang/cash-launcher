4d7f3120c8dbd09902a199aa7a873f061a6f69da
Roc Wang <roc@kiwiberry.nz>
2021-01-13 15:24:25 +1300

    fix: https on dev server

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { homedir } from "os";
-import { readFileSync } from "fs";
+import { existsSync, readFileSync } from "fs";
 
 export default defineConfig({
   plugins: [vue()],
   server: {
     open: true,
-    https:
-      process.env.NODE_ENV === "development"
-        ? {
-            key: readFileSync(homedir() + "/.localhost_ssl/server.key"),
-            cert: readFileSync(homedir() + "/.localhost_ssl/server.crt"),
-          }
-        : false,
+    https: existsSync(`${homedir()}/.localhost_ssl/server.key`)
+      ? {
+          key: readFileSync(`${homedir()}/.localhost_ssl/server.key`),
+          cert: readFileSync(`${homedir()}/.localhost_ssl/server.crt`),
+        }
+      : false,
   },
   optimizeDeps: {
     exclude: ["prettier"],
